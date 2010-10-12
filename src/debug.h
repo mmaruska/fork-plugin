1040f92e3f61f71f3add5b9f80b424d3550095e7
Michal Maruska <mmaruska@gmail.com>
2010-10-12 06:10:27 +0300

    clean

--- src/debug.h
+++ src/debug.h
@@ -5,16 +5,17 @@
 #if DEBUG
 #define MDB(x) if (machine->config->debug) {ErrorF x;}
 
-#define MINFO(x) if (machine->config->debug>1) {ErrorF x;}
+// unused: #define MINFO(x) if (machine->config->debug>1) {ErrorF x;}
+
 // do {if (machine->config->debug == TRUE )
 // {printf("config is%d\n", machine->config->debug); printf x;} }
 // while (0)
+
 #define DB(x)     ErrorF x
 
 #else  /* DEBUG */
 #define DB(x) do { ; } while (0)
 #define MDB(x) do { ; } while (0)
+
 #endif /* DEBUG */
-
-
-#endif
+#endif /* _DEBUG_H_ */
