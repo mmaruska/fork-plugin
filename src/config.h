0ef55b81fc99a3a09a6a719189c2347f19acc177
Michal Maruska <mmaruska@gmail.com>
2010-10-14 03:29:15 +0300

    move colors to debug

--- src/config.h
+++ src/config.h
@@ -2,48 +2,19 @@
 #define CONFIG_H
 
 
-#define XLOADER 1               // this is loaded as a module!
-#define XKB_IN_SERVER 1
-#define PLUGIN_VERSION 32
-#define VERSION_STRING VERSION
-
-
-#if 1                           // i like colored tracing (in 256-color xterm)
-#define escape_sequence "\x1b"
-#define info_color "\x1b[47;31m"
-#define fork_color "\x1b[47;30m"
-#define key_color "\x1b[01;32;41m"
-#define keysym_color "\x1b[33;01m"
-#define warning_color "\x1b[38;5;160m"
-#define key_io_color "\x1b[38;5;226m"
-#define color_reset "\x1b[0m"
-
-#else
-#define info_color ""
-#define key_color  ""
-#define warning_color  ""
-#define color_reset ""
-#define escape_sequence ""
-#endif
-
-
 // i want the version string settable from the command line!!
 // fixme: if it is not a string it's a segfault !!!
-#ifndef VERSION
-#define VERSION "1.1"
+#ifndef VERSION_STRING
+#define VERSION_STRING "(unknown version)"
 #endif
+#define PLUGIN_VERSION 33
 
 
-
-/// Trace + debug:
+// use -DNDEBUG to avoid asserts!
+// Trace + debug:
 #define DEBUG 1
-
-#if (! DEBUG)
-#define NODEBUG 1
-// #include <assert.h>
-#endif
-
-
+// I like colored tracing (in 256-color xterm)
+#define USE_COLORS 1
 
 #define KEEP_PREVIOUS 0
 #define STATIC_LAST 1
--- src/debug.h
+++ src/debug.h
@@ -1,21 +1,36 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
+#include "config.h"
 
 #if DEBUG
-#define MDB(x) if (machine->config->debug) {ErrorF x;}
 
-// unused: #define MINFO(x) if (machine->config->debug>1) {ErrorF x;}
+#if USE_COLORS 
+#define escape_sequence "\x1b"
+#define info_color "\x1b[47;31m"
+#define fork_color "\x1b[47;30m"
+#define key_color "\x1b[01;32;41m"
+#define keysym_color "\x1b[33;01m"
+#define warning_color "\x1b[38;5;160m"
+#define key_io_color "\x1b[38;5;226m"
+#define color_reset "\x1b[0m"
+#else
+#define info_color ""
+#define key_color  ""
+#define warning_color  ""
+#define color_reset ""
+#define escape_sequence ""
+// ....
+#endif
 
-// do {if (machine->config->debug == TRUE )
-// {printf("config is%d\n", machine->config->debug); printf x;} }
-// while (0)
-
+#define MDB(x) do {if (machine->config->debug) {ErrorF x;} } while (0)
 #define DB(x)     ErrorF x
+
 
 #else  /* DEBUG */
 #define DB(x) do { ; } while (0)
 #define MDB(x) do { ; } while (0)
-
 #endif /* DEBUG */
+
+
 #endif /* _DEBUG_H_ */
