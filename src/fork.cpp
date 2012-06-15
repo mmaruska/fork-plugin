560dad4eb65f17b353b1de9ecd0ed1fc8fcbd912
Michal Maruska <mmaruska@gmail.com>
2012-06-15 11:53:31 +0200

    clean C macros a bit

--- src/fork.cpp
+++ src/fork.cpp
@@ -23,12 +23,12 @@
  */
 
 #if USE_LOCKING
-#define CHECK_LOCKED(m) assert(m->lock)
-#define CHECK_UNLOCKED(m) assert(m->lock == 0)
+#define CHECK_LOCKED(m)   assert((m)->lock)
+#define CHECK_UNLOCKED(m) assert((m)->lock == 0)
+// might be: CHECK_UNLOCKED(m)      ((m)->lock==1)
 
-// might be:        (CHECK_UNLOCKED(m),m->lock=1)
-#define LOCK(m)  m->lock=1
-#define UNLOCK(m)  m->lock=0
+#define LOCK(m)    (m)->lock=1
+#define UNLOCK(m)  (m)->lock=0
 #else
 #error "define locking macros!"
 #endif
